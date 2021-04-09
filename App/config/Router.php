a3707b2980977e0ad796d699035cebcb4c8eb2b3
Eloise EIn. INCROCIATI <eloise.incrociati@myrem.lan>
2021-04-09 15:05:40 +0200

    Suppression des commentaires

--- App/config/Router.php
+++ App/config/Router.php
@@ -47,6 +47,9 @@ class Router
                 elseif($route === 'flagComment'){
                     $this->frontController->flagComment($this->request->getGet()->get('commentId'));
                 }
+                elseif($route === 'deleteComment'){
+                    $this->backController->deleteComment($this->request->getGet()->get('commentId'));
+                }
             }
             else{
                 $this->frontController->home();
